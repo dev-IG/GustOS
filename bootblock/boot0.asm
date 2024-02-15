a5c24b1cce754f33ed9a15612d6cad1c38d39d0f
FruitPunchSamurai <sheshan1961@gmail.com>
2024-02-14 23:58:22 -0500

    Finish boot0 and start boot1

--- bootblock/boot0.asm
+++ bootblock/boot0.asm
@@ -2,28 +2,40 @@
 ; @author Dev IG
 
 bits 16     ; We're dealing with 16 bit code
+GLOBAL start ; define global start variable for boot0
 org 0x7c00  ; Inform the assembler of the starting location for this code
-
 start:
-    mov ah, 0x0e   ;Set AH to 0x0e so when we call int 0x10, BIOS calls the write to TTY
+    cli     ; Disable Interrupts
+    ljmp 0, next_line_code ;Long Jump to canonicalize %CS:%EIP
 
-    mov bx, the_secret
-    jmp loop
 
-loop:
-    mov al, [bx]
-    cmp al, 0
-    je  halt
-    int 0x10
-    inc bx
-    jmp loop
+next_line_code:
+    ;Clear the Code, Data, Extra, Stack segments
+    xor ax, ax
+    xor ax, cs     ; Code segment
+    mov ax, ds     ; Data segment
+    mov ax, es     ; Extra segment
+    mov ax, ss     ; Stack segment
 
-halt:
-    jmp $
 
-the_secret:
-    db 'HELLO WORLD', 0
+    ; Step 6: Jump to boot1 code
+    call _start
 
-; Mark the device as bootable
-times 510-($-$$) db 0 ; Add any additional zeroes to make 510 bytes in total
+
+; Prints `ERR: ` and the given error code to screen and hangs.
+; parameter: error code (in ascii) in al
+error_handler:
+    ; Disable interrupts
+    cli
+
+    ; 2. Display error message (for example, printing to screen)
+    mov dword [0xb8000], 0x4f524f45   ; "ERROR"
+    mov dword [0xb8004], 0x4f3a4f52   ; ": OR"
+    mov dword [0xb8008], 0x4f204f20   ; " O "
+    mov byte  [0xb800a], al           ; Print error code (assuming it's in AL)
+
+    ; 3. Permanently suspend progress in execution
+    hlt
+
+%include "boot1.asm"
 dw 0xAA55 ; Magic Number to let BIOS know we are the boot sector
--- bootblock/boot1.asm
+++ bootblock/boot1.asm
@@ -1,7 +1,34 @@
 ; @brief First stage bootloader.
 ; @author Dev IG
-
-
-	.code16
-	.global	_start
+bits 16
+GLOBAL _start ; define global _start variable for boot1
 _start:
+    cli ; Disable interrupts if not already done
+
+
+    call enable_A20 ; Enable A_20 Line
+
+
+    ; TODO: load the global descriptor table
+    ; TODO: Switch to protected mode -> Long Mode?
+    ; TODO: Load segment registers
+    ; TODO: Call bootmain for multiboot loader
+
+    mov start, sp       ; Set stack pointer to 0x7C00 (start from boot0.asm points to 0x7C00)
+
+boot_error_handler:
+    ; Notify the user of a failure condition
+    ; Disable interrupts (if not already disabled)
+    cli
+    mov dword [0xb8000], 0x4f524f45   ; "ERROR"
+    mov dword [0xb8004], 0x4f3a4f52   ; ": OR"
+    mov dword [0xb8008], 0x4f204f20   ; " O "
+    mov dword [0xb800C], 0x4f524f46   ; "PROT" (Protected Mode)
+    mov dword [0xb8010], 0x4f204f20   ; " O "
+    mov dword [0xb8014], 0x4f444e49   ; "INDI" (Indicator)
+    mov byte  [0xb800a], al           ; Print error code (assuming it's in AL)
+    ; Permanently suspend progress in execution
+    hlt
+
+%include "boot0.asm"
+%include "A20.asm"
